7d543c1d5134ad51593f687e0bf14af7c35535e1
«Artem Martirosyan» <amartirosyan@ozon.travel>
2018-07-18 11:24:52 +0300

    Initial Commit

---
+++ CopyCode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CopyCode
+//
+//  Created by Артем on 18/07/2018.
+//  Copyright © 2018 Artem Martirosyan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
