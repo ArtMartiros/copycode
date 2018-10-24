e3e26980258e8775de1e06cf464aca2a7648e183
«Artem Martirosyan» <martiros.artem@gmail.com>
2018-10-24 12:56:43 +0300

    исправил корелятор

--- CopyCode/Recognizer/TextRecognizerManager.swift
+++ CopyCode/Recognizer/TextRecognizerManager.swift
@@ -62,13 +62,13 @@ final class TextRecognizerManager {
 
             let completedBlocks = restoredBlocks.map { wordRecognizer.recognize($0) }
             Timer.stop(text: "WordRecognizer Recognize")
-//                        for block in restoredBlocks {
-//                            if case .grid(let grid) = block.typography {
-//                                let value = CodableHelper.encode(block)
-//            
-//                                print(value)
-//                            }
-//                        }
+            for block in gridBlocks {
+                if case .grid(let grid) = block.typography {
+                    let value = CodableHelper.encode(block)
+
+                    print(value)
+                }
+            }
             //            sself.printAllCustomLetters(from: blocksWithTypes)
             completion(bitmap, completedBlocks, error)
         }
---
+++ CopyCodeTests/GridCorrelatorTests.swift
@@ -0,0 +1,32 @@
+//
+//  GridCorrelatorTests.swift
+//  CopyCodeTests
+//
+//  Created by Артем on 24/10/2018.
+//  Copyright © 2018 Artem Martirosyan. All rights reserved.
+//
+
+import XCTest
+
+class GridCorrelatorTests: XCTestCase {
+    let correlator = GridLineCorrelator()
+
+    func testExample() {
+        let scene = Scene.sc13_p1
+        let block = scene.getGridBlock(self)
+        let bitmap = scene.image.bitmap
+        guard case .grid(let grid) = block.typography else { return }
+        let line = block.lines[3]
+        let letters = line.words.map { $0.letters }.reduce([], +)
+        let arrayOfFrame = grid.getArrayOfFrames(from: block.frame)
+        let correlations = correlator.correlate(letters, frames: arrayOfFrame[7])
+        correlations.forEach { print($0) }
+//        let elementRestorer = MissingElementsRestorer(bitmap: bitmap)
+//        let words = elementRestorer.restoreWords(in: line, lineFrames: arrayOfFrame[7])
+        print("d")
+    }
+
+    func testRestorer() {
+
+    }
+}
